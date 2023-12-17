c311e8ea61c230dfd8dc3fc71b6da657e8f7994b
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2023-12-17 10:18:54 -0500

    Update miryoku_layer_alternatives.h

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -287,10 +287,10 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 
 //Layer for accented char
 #define MIRYOKU_ALTERNATIVES_MOUSE_INVERTEDT_FLIP \
-U_NU,              U_NU,              U_NU,              U_NU,              U_NU,              U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
-U_NU,              U_NU,              U_NU,              U_NU,              U_NU,              U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-U_NU,              U_NU,              U_NU,              U_NU,              U_NU,              U_NA,              &u_to_U_MOUSE,     &u_to_U_SYM,       &kp RALT,          U_NA,              \
-U_NP,              U_NP,              U_NU,              U_NU,              U_NU,              U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+&ac_icir,          &ac_ucir,          &ac_acir,          &ac_ecir,          U_NU,              U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+RA(&kp COMMA),     &ac_ugrv,          &ac_agrv,          &ac_eacu,          &ac_egrv,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&ac_itre,          &ac_utre,          &ac_ocir,          &ac_etre,          U_NU,              U_NA,              &u_to_U_MOUSE,     &u_to_U_SYM,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DEL,           &kp SPACE,         &kp BSPC,          U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_MOUSE_FLIP \
 U_WH_L,            U_WH_D,            U_WH_U,            U_WH_R,            U_NU,              U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
